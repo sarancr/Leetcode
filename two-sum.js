b6da62d1a1f40fc00421c55aca987ae221e5dde4
Saranya Chithalandur Rajendran <sarancr@users.noreply.github.com>
2021-01-05 09:40:23 -0800

    Create two-sum.js

---
+++ two-sum.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/two-sum/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums,target) {
+    
+    
+    const result = [];
+    
+    for(let i= 0; i < nums.length; i++){
+        for(let j= i+1; j<nums.length;j++){
+         if(nums[i] + nums[j] === target){
+             
+             result.push (i);
+             result.push(j);
+             
+         }
+   
+        
+        }
+        }
+    
+    return result;
+}
